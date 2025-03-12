1104d126581504157b92166efb30642e28c7fa7c
LostoVayne <moises.barrera.dev@gmail.com>
2025-03-12 19:41:27 -0300

    corrige error de comando en prisma, elimina --no engine para uso de bd en vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,14 @@
-import { PrismaClient } from '@prisma/client/edge'
-import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from "@prisma/client/edge";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends(withAccelerate())
-}
+  return new PrismaClient();
+};
 
 declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>
-} & typeof global
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
 
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
-export default prisma
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+export default prisma;
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
